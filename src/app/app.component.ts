f7ddd802cd83d1ce576ee2309e331d0974d4c259
Pauljls <paulazarsol@gmail.com>
2024-12-12 21:58:28 -0500

    generando componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from './container/container.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContainerComponent,
+    FormularioComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
