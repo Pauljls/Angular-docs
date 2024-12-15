50ecceda618d6ab6a075aafa71296744c23b2295
Pauljls <paulazarsol@gmail.com>
2024-12-14 21:33:28 -0500

    probando sentencias iterativas y decisivas en angualr

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -10,6 +10,14 @@ import { Component, input, computed, signal } from '@angular/core';
 export class ContainerComponent {
   //usando inputSingal
   myName = signal('pedro');
+  booleanValue = signal(true);
+  myArray = signal([
+    'item 1',
+    'item 2',
+    'item 3',
+  ]);
+
+  myFinalArray = computed(()=> this.myArray)
   //COMPUTED SIRVE APRA MANEJAR ESTADOS PUES GENERA VALROES 
   //EN FUNCION DE OTRAS SEÑALES
   //label = computed(()=>`El valor es ${this.value()}`);
